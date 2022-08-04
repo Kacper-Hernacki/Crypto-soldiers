da5421ea21441444c8d2fe4a313fb5feca051efd
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-08-04 15:04:48 +0200

    feat(crytpozombies) lesson one inspiration

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -2,18 +2,30 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract MyContract {
-    string private name;
+contract Soldiers {
+    uint characteristicDigits = 16;
+    uint characteristicModulus = 10 ** characteristicDigits;
+   
+   struct Soldier {
+    string name;
+    uint characteristic;
+   }
 
-    constructor(string memory _name) {
-        name = _name;
+    Soldier[] public soldiers;
+
+    function _createSoldier(string memory _name, uint _characteristic) private {
+        soldiers.push(Soldier(_name, _characteristic));
     }
 
-    function changeName(string memory _name) public {
-        name = _name;
+
+    function _generateRandomCharacteristic(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % characteristicModulus;
     }
 
-    function getName() public view returns (string memory) {
-        return name;
+       function createRandomSoldier(string memory _name) public {
+        uint randCharacteristic = _generateRandomCharacteristic(_name);
+        _createSoldier(_name, randCharacteristic);
     }
+
 }
